d179e5848b962b50ed560ff30e8982aabf12b0f9
haslersn <sebastian.hasler@gmx.net>
2018-11-26 16:19:55 +0100

    crow-cycle.py: Combine src/cycle.py, src/tex.py and latexmk
    `crow-cycle.py` is a new script that basically executes:
    ```
    mkdir build
    src/cycle.py -i <input> | src/tex.py -o build/crow-cycle.tex --desc <desc>
    cd build
    latexmk --pdf crow-cycle.tex
    mv crow-cycle.pdf ..
    cd ..
    rm -rf build
    ```
    It tries to do that in a platform independent way, using python library functions.

---
+++ crow-cycle.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+import shutil
+import subprocess
+import sys
+
+# If you change the arguments, please update the usage section in README.md
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('-i', '--input',
+                    help='Path to the file containing names for creating a circle of "murderer-murdered" pairs. '
+                         'Only one name per line is allowed. Names may contain spaces. '
+                         'If not specified, the input is read from the standard input.')
+parser.add_argument('-o', '--output',
+                    help='Path to the output file. Output is pdf.',
+                    default='crow-cycle.pdf')
+parser.add_argument('--desc',
+                    help='Description that is printed in every table row.')
+args = parser.parse_args()
+
+# mkdir -p build
+if not os.path.exists('build'):
+    os.mkdir('build')
+
+# src/cycle.py -i <input> | src/tex.py -o build/crow-cycle.tex
+cycle_cmd = ['src/cycle.py']
+if args.input is not None:
+    cycle_cmd += ['-i', args.input]
+tex_cmd = ['src/tex.py', '-o', 'build/crow-cycle.tex']
+if args.desc is not None:
+    tex_cmd += ['--desc', args.desc]
+cycle_pipe = subprocess.Popen(cycle_cmd, stdout=subprocess.PIPE)
+subprocess.run(tex_cmd, stdin=cycle_pipe.stdout)
+cycle_pipe.wait()
+
+# cd build; latexmk --pdf crow-cycle.tex; cd ..
+subprocess.run(['latexmk', '--pdf', 'crow-cycle.tex'], cwd='build')
+
+# mv build/output.pdf <output>
+os.rename('build/crow-cycle.pdf', args.output)
+
+# rm -rf build
+shutil.rmtree('build')
