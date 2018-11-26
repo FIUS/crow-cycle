79a75c9a58a3f26e7969f103c049df8c2f9de836
Matthias Hermann <matthias.h@online.de>
2018-11-26 08:12:22 +0100

    Implement argument parsing with 'argparse'

--- src/cycle.py
+++ src/cycle.py
@@ -1,1 +1,11 @@
 #!/usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument('names',
+                    type=argparse.FileType('r'),
+                    help='Path to a file containing names for creating a circle of "murderer-murdered" pairs.'
+                         'Only one name per line is allowed. Names may contain spaces.')
+
+args = parser.parse_args()
