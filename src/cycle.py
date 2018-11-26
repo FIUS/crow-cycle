7967f9562578f1b44e1e47870d574d240d6ea9d6
Matthias Hermann <matthias.h@online.de>
2018-11-26 08:18:53 +0100

    Read names from text file w/o trailing whitespaces

--- src/cycle.py
+++ src/cycle.py
@@ -9,3 +9,20 @@ parser.add_argument('names',
                          'Only one name per line is allowed. Names may contain spaces.')
 
 args = parser.parse_args()
+
+
+def read_names_from_file(file):
+    """
+    Reads the provided file line by line to provide a list representation of the contained names.
+    :param file: A text file containing one name per line
+    :return: A list of the names contained in the provided text file
+    """
+    names_list = []
+
+    for line in file:
+        names_list.append(str(line).strip())
+
+    return names_list
+
+
+names_list = read_names_from_file(args.names)
